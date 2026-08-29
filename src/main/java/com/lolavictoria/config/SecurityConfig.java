47c6894f8092a66d742acded1a17188816ea8b50
LolaVictoria <damilolaoniyide11@gmail.com>
2026-08-29 23:52:41 +0100

    added deployed frontend link to cors

--- src/main/java/com/lolavictoria/config/SecurityConfig.java
+++ src/main/java/com/lolavictoria/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
             "http://localhost:5173"
-            //"https://your-frontend-domain.vercel.app"
+            "https://study-tracker-v1.vercel.app/login"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
